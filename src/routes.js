d268862bb6743a982118e8c1489d508c8c3cd7d3
jiayueming <529228544@qq.com>
2017-09-24 20:03:57 +0800

    上传代码

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import form from './components/header.vue'
+import computed from './components/computed.vue'
+import watched from './components/watched.vue'
+
+//注册
+Vue.use(Router);
+
+export default new Router({
+    routes:[
+        {
+            path: '/',
+            component: form,
+            name: 'form'
+        },
+        {
+            path: '/form',
+            component: form,
+            name: 'form'
+        },
+        {
+            path: '/computed',
+            component: computed,
+            name: 'computed'
+        },
+        {
+            path: '/watched',
+            component: watched,
+            name: 'watched'
+        },
+    ]
+})
